3c6ca765fcc89f3e00bb8572866c25c1a3b39543
Arnie97 <arnie97@gmail.com>
2017-09-22 21:30:07 +0800

    Allow slashes in train model patterns.

--- dump.py
+++ dump.py
@@ -6,16 +6,24 @@ import json
 
 
 def main(argv):
-    pattern = r'新?CR[\w-]+型(重联)?'
+    pattern = r'新?CR[\w/-]+型(?:重联)?'
     with open(argv[0]) as f:
         lst = {}
         for line in f:
             code, _, model = line.partition(' ')
-            model = re.match(pattern, model)[0]
+            try:
+                models = re.findall(pattern, model)
+                assert len(models) == 1
+            except:
+                print(line)
+            else:
+                model = models[0]
             if model not in lst:
                 lst[model] = []
             lst[model].append(code)
 
+    print(len(lst), 'models found:')
+    print('\n'.join(sorted(lst.keys())))
     with open(argv[1], 'w') as f:
         json.dump(lst, f)
 

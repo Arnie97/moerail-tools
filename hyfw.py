cc6062b0b6c9129555a9b85ee8872590d149a8f4
Arnie97 <arnie97@gmail.com>
2018-04-14 22:15:45 +0800

    List all the stations recursively.

---
+++ hyfw.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+from string import ascii_uppercase as alphabet
+from typing import List, Dict
+
+
+def stations(pinyin: str) -> List[Dict[str, str]]:
+    'Get all the stations from 95306.'
+    # http://www.12306.cn/mormhweb/hyfw/hyckcx/
+    url = 'http://dynamic.12306.cn/yjcx/doPickJZM'
+    params = dict(param=pinyin, type=1, czlx=0)
+    response = requests.post(url, params)
+    return json.loads(response.text)
+
+
+def dfs(pinyin='') -> List[Dict[str, str]]:
+    'Load recursively when the API limit is exceeded.'
+    results = stations(pinyin)
+    if len(results) < 100:
+        return results
+    else:
+        return sum((dfs(pinyin + c) for c in alphabet), [])
+
+
+if __name__ == '__main__':
+    while True:
+        results = dfs(input('> '))
+        for r in results:
+            print(str(r).replace("'", ''))
+        print('=', len(results))

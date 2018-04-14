3a6dea9b0c7120ca91d2c93cb7cc8d533e9b72c5
Arnie97 <arnie97@gmail.com>
2018-04-14 21:41:45 +0800

    Split bulk requests into chunks.

--- tmis.py
+++ tmis.py
@@ -5,11 +5,11 @@ import requests
 from collections import OrderedDict
 
 
-def tmis(name='') -> OrderedDict:
+def tmis(name='', bureau=0) -> OrderedDict:
     url = 'http://hyfw.12306.cn/hyinfo/action/FwcszsAction_getljcz'
-    params = 'limit timestamp sheng shi ljdm'
+    params = 'limit timestamp sheng shi'
     params = {k: '' for k in params.split()}
-    params['q'] = name
+    params.update(q=name, ljdm=format(bureau, '02'))
     response = requests.post(url, params)
     list_of_dicts = json.loads(response.text)
     return OrderedDict((d['HZZM'], d['TMISM']) for d in list_of_dicts)
@@ -17,5 +17,11 @@ def tmis(name='') -> OrderedDict:
 
 if __name__ == '__main__':
     while True:
-        for k, v in tmis(input('> ')).items():
+        name = input('> ')
+        results = tmis(name)
+        if len(results) == 50:
+            for i in range(1, 19):
+                results.update(tmis(name, i))
+        for k, v in results.items():
             print(k, v, sep='\t')
+        print('=', len(results))

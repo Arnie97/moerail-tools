d55399598b7a032b3164ca23e9addb4375bb9d5e
Arnie97 <arnie97@gmail.com>
2018-05-13 20:01:03 +0800

    Refactor the grouping module.

--- group.py
+++ group.py
@@ -2,30 +2,35 @@
 
 import re
 import json
+from typing import Dict, TextIO
 
 from util import argv, open
+PATTERN = r'新?CR[\w/-]+型(?:重联)?'
+
+
+def group(file: TextIO) -> Dict[str, list]:
+    'Group the train routes by the vehicle model used.'
+    lst = {}
+    for line in file:
+        code, _, model = line.partition(' ')
+        try:
+            models = re.findall(PATTERN, model)
+            assert len(models) == 1
+        except:
+            print(line)
+        else:
+            model = models[0]
+        if model not in lst:
+            lst[model] = []
+        lst[model].append(code)
+    return lst
 
 
 def main(src, dest):
-    'Group the train routes by the vehicle model used.'
-    pattern = r'新?CR[\w/-]+型(?:重联)?'
     with open(src) as f:
-        lst = {}
-        for line in f:
-            code, _, model = line.partition(' ')
-            try:
-                models = re.findall(pattern, model)
-                assert len(models) == 1
-            except:
-                print(line)
-            else:
-                model = models[0]
-            if model not in lst:
-                lst[model] = []
-            lst[model].append(code)
-
-    print(len(lst), 'models found:')
+        lst = group(f)
     print('\n'.join(sorted(lst.keys())))
+    print(len(lst), 'models found.')
     with open(dest, 'w') as f:
         json.dump(lst, f)
 
